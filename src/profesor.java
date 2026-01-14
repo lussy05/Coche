5a1e618657f7a53c20e5d7490fd4b5401ccc4a8a
Lucia Garcia Perez <lgp0029@alu.medac.es>
2026-01-14 14:33:50 +0100

    Cambios de nomnbre para funcionar

--- src/profesor.java
+++ src/profesor.java
@@ -2,13 +2,12 @@ public class Profesor {
     // si aqui no pones contructor
     // esto que hemos puesto funcionaria ; // profesor pablo = new profesor();
 
-
     // esto son atributos:
     public String dni;
     public String nombre;
     public double salario;
 
-//como se crea el constructor, entre parentesis el orden e informacion
+    // como se crea el constructor, entre parentesis el orden e informacion
     public Profesor(String dniprofesor, String nombreprofesor, double salarioprofesor) {
         dni = dniprofesor;
         nombre = nombreprofesor;
